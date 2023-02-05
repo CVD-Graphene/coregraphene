41468a717cbf401ff0a8f1b0c6caf74304819e5f
DanMitroshin <alfocintavr@mail.ru>
2023-02-05 18:44:08 +0300

    Update communicators

---
+++ components/communicators/base.py
@@ -0,0 +1,116 @@
+from components.communication_methods import BaseCommunicationMethod
+from constants import COMMUNICATION_INTERFACE_STATUS
+
+from exceptions.communicators import (
+    SetupCommunicatorException,
+    InactiveCommunicatorException,
+    BaseCommunicatorException,
+    DoubleDeclarationMethodCommunicatorException,
+)
+
+from exceptions.communication_methods import BaseCommunicationMethodException
+
+
+class AbstractCommunicator(object):
+    communication_method_class = None
+
+    def __init__(
+            self,
+            speed=None,
+            port=None,
+            communication_method: BaseCommunicationMethod = None
+    ):
+        if communication_method is not None and\
+                self.communication_method_class is not None:
+            raise DoubleDeclarationMethodCommunicatorException
+
+        if communication_method is not None:
+            self.communication_method: BaseCommunicationMethod = communication_method
+
+        elif self.communication_method_class is not None:
+            self.communication_method: BaseCommunicationMethod = self.communication_method_class()
+
+        self.speed = speed
+        self.port = port
+
+        self._status = COMMUNICATION_INTERFACE_STATUS.INACTIVE
+        self._errors = []
+        # self.setup_configuration()
+
+        self.communicator_id = self.__class__.__name__
+
+    def setup(self):
+        try:
+            self._status = COMMUNICATION_INTERFACE_STATUS.ACTIVE
+            self.communication_method.setup()
+        except BaseCommunicationMethodException:
+            raise
+        except Exception as e:
+            self._errors.append(e)
+            self._status = COMMUNICATION_INTERFACE_STATUS.HAS_ERRORS
+            raise SetupCommunicatorException(
+                communicator_id=self.communicator_id
+            ) from e
+
+    def is_valid(self, raise_exception=True):
+        e = None
+        if self._status == COMMUNICATION_INTERFACE_STATUS.INACTIVE:
+            e = InactiveCommunicatorException(communicator_id=self.communicator_id)
+            self._errors.append(e)
+        elif self._status == COMMUNICATION_INTERFACE_STATUS.HAS_ERRORS:
+            if bool(self._errors):
+                e = self._errors[-1]
+            else:
+                self._status = COMMUNICATION_INTERFACE_STATUS.ACTIVE
+
+        if e is not None and raise_exception:
+            raise e
+
+        return not bool(self._errors)
+
+    def send(self, value, raise_exception=True):
+        """
+        preprocess value -> send value -> get answer -> answer processing ->
+        1. has mistakes -> raise error
+        2. all oKey -> return value
+        :return:
+        """
+        self.is_valid(raise_exception=raise_exception)
+
+        try:
+            preprocessing_value = self._preprocessing_value(value)
+            answer = self.communication_method.send(preprocessing_value)
+
+            return self._postprocessing_value(answer)
+        except BaseCommunicationMethodException:
+            raise
+        except Exception as e:
+            return self._handle_exception(e)
+
+    def read(self, raise_exception=True):
+        self.is_valid(raise_exception=raise_exception)
+
+        try:
+            answer = self.communication_method.read()
+            return self._postprocessing_value(answer)
+        except BaseCommunicationMethodException:
+            raise
+        except Exception as e:
+            return self._handle_exception(e)
+
+    def _preprocessing_value(self, value):
+        return value
+
+    def _postprocessing_value(self, value):
+        """
+        Is answer is oKey?
+        1. Answer OK ? -> _extract_value
+        2. Else -> _handle_exception
+        :return:
+        """
+        return value
+
+    def _handle_exception(self, e):
+        raise BaseCommunicatorException(
+            communicator_id=self.communicator_id
+        ) from e
