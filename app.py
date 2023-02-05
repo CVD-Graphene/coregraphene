df30524eb6acca74780a58fbbdee361864dce20b
DanMitroshin <alfocintavr@mail.ru>
2023-02-05 21:13:04 +0300

    Add constants with modules files

---
+++ app.py
@@ -0,0 +1,8 @@
+import os, datetime
+
+# from Tests.devices.current_source_akip import test_akip_2, get_serial_port
+# from Tests.devices.trm200 import test_trm_2, test_4, test_1, check_port
+# from Tests.devices.rrg import test_rrg_3
+# from Tests.devices.vakumetr import test_1, test_2
+
+os.environ["QT_IM_MODULE"] = "qtvirtualkeyboard"
---
+++ constants/__init__.py
@@ -0,0 +1,5 @@
+from .NOTIFICATIONS import *
+from .DEVICE_STATUS import *
+from .COMMUNICATION_INTERFACE_STATUS import *
+from .ACTION_NAMES import *
+from .RECIPE_STATES import *
