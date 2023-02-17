c16597509f4b304759c49969804995187dfa8004
DanMitroshin <alfocintavr@mail.ru>
2023-02-17 23:02:45 +0300

    Update settings

--- components/communication_methods/modbus.py
+++ components/communication_methods/modbus.py
@@ -55,7 +55,7 @@ class ModbusCommunicationMethod(BaseCommunicationMethod):
         self.instrument = mm.Instrument(
             self.port,
             self.instrument_number,
-            close_port_after_each_call=True,
+            close_port_after_each_call=False,
             mode=self.mode,
             debug=False
         )
