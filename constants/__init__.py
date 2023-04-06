87a9b72abd18be4077c471f2c1e97ab1993dc45b
DanMitroshin <alfocintavr@mail.ru>
2023-04-06 22:56:23 +0300

    Restructure all valves components

--- constants/__init__.py
+++ constants/__init__.py
@@ -2,3 +2,9 @@ from .NOTIFICATIONS import *
 from .DEVICE_STATUS import *
 from .COMMUNICATION_INTERFACE_STATUS import *
 from .RECIPE_STATES import *
+
+RECIPE_STATES_TO_STR = {
+    RECIPE_STATES.RUN: "Running",
+    RECIPE_STATES.PAUSE: "Pause",
+    RECIPE_STATES.STOP: "Stop",
+}
