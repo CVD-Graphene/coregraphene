43ad69422e3c574eac882b5be348028b1cf1073b
DanMitroshin <alfocintavr@mail.ru>
2023-04-14 12:12:00 +0300

    Update sending value

--- components/communicators/adc_dac.py
+++ components/communicators/adc_dac.py
@@ -6,14 +6,16 @@ from ...utils import int2base
 class AdcDacCommunicator(AbstractCommunicator):
     communication_method_class = SpidevCommunicationMethod
 
-    def _common_preprocessing_value(self, address=0, value=0):
+    def _common_preprocessing_value(self, address=0, value=0, sending=False):
         # print("R4")
         n = 8
 
         device_base = int2base(address, base=2)
-        value_base = int2base(min(1023, value), base=2)
+        value_base = int2base(min(2**12 - 1, value), base=2)
+        start_bit = "0" if sending else "0"
+        add_str = "" if sending else ("0" * n * 2)
         # print("Bases:", device_base, address, value_base, value)
-        data_str = f"1{device_base.zfill(3)}{value_base.zfill(10)}00" + "0" * n * 2
+        data_str = f"{start_bit}{device_base.zfill(3)}{value_base.zfill(12)}" + add_str
 
         # print("D1:", len(data_str))
         data = [int(data_str[i:i + n], 2) for i in range(0, len(data_str), n)]
@@ -21,11 +23,11 @@ class AdcDacCommunicator(AbstractCommunicator):
         return {"data": data}
 
     def _preprocessing_value(self, **kwargs) -> dict:
-        return self._common_preprocessing_value(**kwargs)
+        return self._common_preprocessing_value(sending=False, **kwargs)
 
     def _preprocessing_read_value(self, **kwargs) -> dict:
         # print("R3")
-        return self._common_preprocessing_value(**kwargs)
+        return self._common_preprocessing_value(sending=True, **kwargs)
 
     def _postprocessing_value(self, value=None):
         try:
