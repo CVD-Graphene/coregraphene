852bb2558ba97065e5fb0d59adb8a8b391439343
DanMitroshin <alfocintavr@mail.ru>
2023-05-02 15:38:14 +0300

    Base arguments update with preparing function

--- auto_actions/arguments.py
+++ auto_actions/arguments.py
@@ -18,17 +18,19 @@ class Argument:
         except Exception as e:
             return f"{str(e)}"
 
-    @abstractmethod
     def _check(self, value):
-        pass
+        self.prepare_value(value)
+
+    def prepare_value(self, value):
+        return value
 
 
 class FloatArgument(Argument):
     arg_type = float
     arg_default = 0.0
 
-    def _check(self, value):
-        value = float(value)
+    def prepare_value(self, value):
+        return float(value)
 
 
 class SccmArgument(FloatArgument):
@@ -40,10 +42,13 @@ class SccmArgument(FloatArgument):
         self.max_sccm = max_sccm
 
     def _check(self, value):
-        value = float(value)
+        value = self.prepare_value(value)
         # range_list = [0, self.max_sccm]
         # if not range_list[0] <= value <= range_list[1]:
         #     raise Exception(f"SCCM value {value} not in range {range_list}")
+
+    def prepare_value(self, value):
+        return float(value)
 
 
 class IntKeyArgument(Argument):
@@ -51,13 +56,13 @@ class IntKeyArgument(Argument):
     arg_type = int
     arg_default = 0
 
-    def _check(self, value):
-        value = int(value)
+    def prepare_value(self, value):
+        return int(value)
 
 
 class PositiveIntKeyArgument(IntKeyArgument):
     def _check(self, value):
-        value = int(value)
+        value = self.prepare_value(value)
         if value < 0:
             raise Exception(f"Int value {value} must be >= 0")
 
@@ -67,17 +72,17 @@ class FloatKeyArgument(Argument):
     arg_type = float
     arg_default = 0.0
 
-    def _check(self, value):
-        value = float(value)
+    def prepare_value(self, value):
+        return float(value)
 
 
-class PositiveFloatKeyArgument(Argument):
+class PositiveFloatKeyArgument(FloatKeyArgument):
     key = "float"
     arg_type = float
     arg_default = 0.0
 
     def _check(self, value):
-        value = float(value)
+        value = self.prepare_value(value)
         if value < 0:
             raise Exception(f"Float value {value} must be >= 0")
 
@@ -87,9 +92,12 @@ class GasListArgument(Argument):
     arg_list = settings.GAS_LIST
 
     def _check(self, value):
-        value = str(value).strip()
+        value = self.prepare_value(value)
         if value not in self.arg_list:
             raise Exception(f"Gas {value} not in gas list")
+
+    def prepare_value(self, value):
+        return str(value).strip()
 
 
 class ValveListArgument(Argument):
@@ -97,16 +105,25 @@ class ValveListArgument(Argument):
     arg_list = settings.VALVE_LIST
 
     def _check(self, value):
-        value = str(value).strip()
+        value = self.prepare_value(value)
         if value not in self.arg_list:
             raise Exception(f"Valve {value} not in valve list")
+
+    def prepare_value(self, value):
+        return str(value).strip()
 
 
 class TimeEditArgument(Argument):
     key = "time"
 
-    def _check(self, value):
-        pass
+    def prepare_value(self, value):
+        if type(value) in [int, float]:
+            return value
+        value = str(value)
+        ints = list(map(int, value.strip().split(':')))
+        if len(ints) == 1:
+            return ints[0]
+        return ints[0] * 60 + ints[1]
 
 
 def get_total_seconds_from_time_arg(time_arg):
