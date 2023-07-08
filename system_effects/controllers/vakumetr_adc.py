cc38e54183f54699e4f570e381b300f5e651217f
DanMitroshin <alfocintavr@mail.ru>
2023-07-08 18:17:37 +0300

    Tmp remove logs for vakumetr

--- system_effects/controllers/vakumetr_adc.py
+++ system_effects/controllers/vakumetr_adc.py
@@ -9,7 +9,7 @@ LOCAL_MODE = settings.LOCAL_MODE
 class GetCurrentPressureVakumetrAdcControllerEffect(ManyDeviceControllerEffect):
 
     def _on_get_value(self, value):
-        print("Vakumetr value:", self._controller._last_thread_command.device_num, value)
+        # print("Vakumetr value:", self._controller._last_thread_command.device_num, value)
         self._controller.current_pressures[self._controller._last_thread_command.device_num] = value
 
     def _call_function(self, value):
