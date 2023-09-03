dccc31a980171d8995d5c4fef999a52c0d0cba33
DanMitroshin <alfocintavr@mail.ru>
2023-09-04 00:20:13 +0300

    Current sccm correct value

--- system_effects/controllers/rrg.py
+++ system_effects/controllers/rrg.py
@@ -40,6 +40,17 @@ class GetCurrentSccmRrgAdcControllerEffect(ManyDeviceControllerEffect):
 
 class GetCurrentSccmRrgBhControllerEffect(ManyDeviceControllerEffect):
     def _on_get_value(self, value):
+        self._controller.current_sccms[self._controller._last_thread_command.device_num] = value
+
+    def _call_function(self, value):
+        # print('====== _call_function', value)
+        if LOCAL_MODE:
+            return 0.0
+        if type(value) == list:
+            value = value[0]
+        return self._process_ssm_value(float(value))
+
+    def _process_ssm_value(self, value):
         # device_num = self._controller._last_thread_command.kwargs['arg1']
         device_num = self._controller._last_thread_command.device_num
         voltage_ratio = self._controller._rrgs_config[device_num]['CONTROLLER_VOLTAGE_RATIO']
@@ -48,12 +59,4 @@ class GetCurrentSccmRrgBhControllerEffect(ManyDeviceControllerEffect):
         max_sccm = self._controller.get_max_sccm_device(device_num=device_num)
         # value == current voltage from [0, max (5)]
         current_sccm = value / self._controller.max_rrg_voltage * max_sccm / voltage_ratio + sccm_shift
-        self._controller.current_sccms[device_num] = current_sccm
-
-    def _call_function(self, value):
-        # print('====== _call_function', value)
-        if LOCAL_MODE:
-            return 0.0
-        if type(value) == list:
-            value = value[0]
-        return float(value)
+        return current_sccm
